b19040e19d2a5e25512a685aa79a3c21a9fb10e3
Eunha Hwang <Eunha Hwang@DESKTOP-MD5JUOL>
2019-04-22 19:57:54 +0900

    메인 화면

--- Project_EYE/src/eye/main/Main.java
+++ Project_EYE/src/eye/main/Main.java
@@ -1,25 +1,31 @@
 package eye.main;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Font.loadFont(getClass().getResourceAsStream("a家唱公B.ttf"), 10);
+			Font.loadFont(getClass().getResourceAsStream("a家唱公L.ttf"), 10);
+			Font.loadFont(getClass().getResourceAsStream("a家唱公M.ttf"), 10);
+
+			Parent root = FXMLLoader.load(getClass().getResource("main_page.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Main Page");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
