e6c66fc685205b42e60afeb5a73a8900b2543cfb
user <user@DESKTOP-74230T4>
2019-05-13 19:57:17 +0900

    지그재그 색 변환 결과값 확인됩니다.

--- Project_EYE/src/eye/game/eyeMovement2/EndPopUpcontroller.java
+++ Project_EYE/src/eye/game/eyeMovement2/EndPopUpcontroller.java
@@ -1,39 +1,64 @@
 package eye.game.eyeMovement2;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class EndPopUpcontroller  {
+public class EndPopUpcontroller {
 
 	@FXML
-	private Button closeBtn;
+	private AnchorPane acpane;
 
 	@FXML
-	private Button restartPopUpBtn;
+	private Button Equal;
 
 	@FXML
 	private TextField answer;
 
 	@FXML
-	void closePopUp(ActionEvent event) {
+	void answerEqual(ActionEvent event) {
+		String getAnswer = answer.getText();
+		int ganswer = Integer.parseInt(getAnswer);
 
-	}
+		if (Playcontroller.cnt == ganswer) {
+			System.out.println(ganswer);
+		} else {
+			System.out.println(Playcontroller.cnt);
 
-	@FXML
-	void numGameRestart(ActionEvent event) {
+		}
 
 	}
 
 	public void ans() {
-		String getAnswer = answer.getText();
-		int ganswer = Integer.parseInt(getAnswer);
-		if (Playcontroller.cnt == ganswer) {
 
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("gameSuccess.fxml"));
+			Scene scene = new Scene(root);
+			Stage primaryStage = (Stage) acpane.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
+
 	}
 
+	public void ans2() {
+
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("gameFail.fxml"));
+			Scene scene = new Scene(root);
+			Stage primaryStage = (Stage) acpane.getScene().getWindow();
+			primaryStage.setScene(scene);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
 
 }
